ffa553743761bb560ca6faf42c6ae741e6e30364
guochao <463561248@qq.com>
2022-04-26 00:53:37 +0800

    update

---
+++ layouts/components.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        sss
+        <slot />
+    </div>
+</template>
--- pages/saber/index.vue
+++ pages/saber/index.vue
@@ -1,9 +1,15 @@
-<script lang="ts" setup>
-import Button from "./button.md";
-</script>
-
 <template>
-  <div>
-    <Button />
+  <div class="p-4">
+    Custom layout defined with <code>definePageMeta</code>
+    <br>
+    <NuxtLink to="/">
+      Back to home
+    </NuxtLink>
   </div>
 </template>
+
+<script>
+definePageMeta({
+  layout: 'components'
+})
+</script>
