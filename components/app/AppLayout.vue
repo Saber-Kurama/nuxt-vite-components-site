2a8944dd318c7e3552e9f26960ff29e2707a88f0
guochao <463561248@qq.com>
2023-01-17 22:42:33 +0800

    ✨ feat:  引入 arco-design

--- components/app/AppLayout.vue
+++ components/app/AppLayout.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="w-full">appLayout</div>
+  <div class="w-full">
+    appLayout
+    <HeaderNotification />
+  </div>
   <div><slot /></div>
   <div>sss</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,39 @@
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ArcoResolver } from "unplugin-vue-components/resolvers";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxt/content"],
+  // 会有一些警告  WARN  Two component files resolving to the same name AUpload:
+  // components: {
+  //   dirs: [
+  //     "~/components",
+  //     {
+  //       prefix: "a",
+  //       extensions: ["vue", "ts", "js"],
+  //       path: "~/node_modules/@arco-design/web-vue/es",
+  //     },
+  //   ],
+  // },
   content: {
     // https://content.nuxtjs.org/api/configuration
+  },
+  vite: {
+    plugins: [
+      AutoImport({
+        resolvers: [ArcoResolver()],
+      }),
+      Components({
+        resolvers: [
+          ArcoResolver({
+            sideEffect: true,
+          }),
+        ],
+      }),
+    ],
+    optimizeDeps: {
+      exclude: ["fsevents"],
+    },
   },
   css: ["~/assets/css/main.css"],
   postcss: {
